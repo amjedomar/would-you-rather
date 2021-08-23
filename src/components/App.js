4e58893080b41c177d8cdd27012238431dfc8316
Amjed Omar <amjedomar201@gmail.com>
2021-08-23 13:51:27 +0300

    Initialize project using Create React App

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import {Component} from 'react';
+import Copyright from './Copyright';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Would You Rather
+        <Copyright />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Copyright.js
@@ -0,0 +1,15 @@
+const Copyright = () => {
+  return (
+    <div className="copyright-wrapper">
+      Icons made by
+      {' '}
+      <a className="link" href="https://www.freepik.com" title="Freepik">Freepik</a>
+      {' '}
+      from
+      {' '}
+      <a className="link" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+    </div>
+  );
+};
+
+export default Copyright;
