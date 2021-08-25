ae876d6ce9d7d1fa0638c1092ca63471aadd26e9
Amjed Omar <amjedomar201@gmail.com>
2021-08-25 23:03:23 +0300

    chore: remove unused code

--- src/actions/authedUser.js
+++ src/actions/authedUser.js
@@ -1,11 +1,6 @@
 export const SET_AUTHED_USER = 'SET_AUTHED_USER';
-export const UNSET_AUTHED_USER = 'UNSET_AUTHED_USER';
 
 export const setAuthedUser = (id) => ({
   type: SET_AUTHED_USER,
   id
-});
-
-export const unsetAuthedUser = () => ({
-  type: UNSET_AUTHED_USER
 });
